0ac30c680042f1f6760f3180b4a667c3a6920fe9
DoniRamadhan <95742544+Donigmg@users.noreply.github.com>
2022-02-17 18:09:12 +0700

    Update song.py

--- Music/Plugins/song.py
+++ Music/Plugins/song.py
@@ -290,8 +290,8 @@ def search_markup2(ID6, ID7, ID8, ID9, ID10, duration6, duration7, duration8, du
 def gets(videoid, user_id):
     buttons= [
             [
-                InlineKeyboardButton(text="Get Audio", callback_data=f'gets audio|{videoid}|{user_id}'),
-                InlineKeyboardButton(text="Get Video", callback_data=f'gets video|{videoid}|{user_id}')
+                InlineKeyboardButton(text="🎙 Get Audio", callback_data=f'gets audio|{videoid}|{user_id}'),
+                InlineKeyboardButton(text="🎥 Video", callback_data=f'gets video|{videoid}|{user_id}')
             ],
             [
                 InlineKeyboardButton(text="🗑 Close Menu", callback_data=f'close2')
